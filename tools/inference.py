39ccd42973bec2937cc6c705784fe857354d7ec7
David Russell <djrussell@ucdavis.edu>
2025-05-22 16:36:43 +0000

    Refactor train and inference scripts to provide a top-level function that can be called externally

--- tools/inference.py
+++ tools/inference.py
@@ -13,7 +13,7 @@ def parse_args():
     parser.add_argument("checkpoint_path", type=Path)
     parser.add_argument("image_folder", type=Path)
     parser.add_argument("output_folder", type=Path)
-    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--batch-size", type=int, default=4)
     parser.add_argument("--store-probs", action="store_true")
     parser.add_argument("--extension", default="")
 
@@ -45,6 +45,7 @@ def save_result(result, output_file, store_probs, orientation):
         output_file = output_file.with_suffix(".png")
         imwrite(output_file, seg)
 
+
 def get_image_shape_orientation(file):
     """Return the (h, w) tuple of image shape if it's a image, otherwise None"""
     try:
@@ -55,37 +56,54 @@ def get_image_shape_orientation(file):
     except:
         return None
 
-if __name__ == "__main__":
-    args = parse_args()
 
+def main(
+    config_path,
+    checkpoint_path,
+    image_folder,
+    output_folder,
+    batch_size=4,
+    store_probs=False,
+    extension="",
+):
     # Get all files
-    search_string = str(Path(args.image_folder, "**" + args.extension))
+    search_string = str(Path(image_folder, "**" + extension))
     all_files = glob.glob(search_string, recursive=True)
-    all_files = list(filter( lambda x: Path(x).is_file(), all_files ))
+    all_files = list(filter(lambda x: Path(x).is_file(), all_files))
     # Get the shapes and orientation of all images. Will be None if not an image
     # This step may be slow
-    image_shapes_and_orientation = [get_image_shape_orientation(file) for file in all_files]
+    print("Reading image shapes and orientations. This step can be slow")
+    image_shapes_and_orientation = [
+        get_image_shape_orientation(file) for file in all_files
+    ]
+    print("Done reading shapes and orientations")
     # Merge the shapes with the paths
     shape_orientation_file_list = zip(image_shapes_and_orientation, all_files)
     # Filter out the tuples that don't correspond to an image
-    shape_orientation_file_list = list(filter(lambda x: x[0] is not None, shape_orientation_file_list))
+    shape_orientation_file_list = list(
+        filter(lambda x: x[0] is not None, shape_orientation_file_list)
+    )
     # Get the unique shapes across all images
-    unique_shapes = np.unique([shape_file[0][0] for shape_file in shape_orientation_file_list], axis=0)
+    unique_shapes = np.unique(
+        [shape_file[0][0] for shape_file in shape_orientation_file_list], axis=0
+    )
     # Convert back into tuples
     unique_shapes = [tuple(unique_shape) for unique_shape in unique_shapes]
-    model = init_model(str(args.config_path), str(args.checkpoint_path))
+    model = init_model(str(config_path), str(checkpoint_path))
 
     print(f"Unique image shapes are {unique_shapes}")
 
     for unique_shape in unique_shapes:
         print(f"Processing images with {unique_shape} shape")
         # Extract the filenames corresponding to images of that shape
-        matching = list(filter(lambda x: x[0][0] == unique_shape, shape_orientation_file_list))
+        matching = list(
+            filter(lambda x: x[0][0] == unique_shape, shape_orientation_file_list)
+        )
 
         n_files = len(matching)
-        for i in range(0, len(matching), args.batch_size):
+        for i in range(0, len(matching), batch_size):
             print(f"index: {i}/{n_files}", end="\r")
-            batching = matching[i : i + args.batch_size]
+            batching = matching[i : i + batch_size]
 
             # Get the file names
             files = [x[1] for x in batching]
@@ -93,9 +111,20 @@ if __name__ == "__main__":
 
             results = inference_model(model, [str(x) for x in files])
 
-            rel_paths = [Path(x).relative_to(Path(args.image_folder)) for x in files]
-            output_files = [Path(args.output_folder, rel_path) for rel_path in rel_paths]
-            for result, output_file, orientation in zip(results, output_files, orientations):
+            rel_paths = [Path(x).relative_to(Path(image_folder)) for x in files]
+            output_files = [Path(output_folder, rel_path) for rel_path in rel_paths]
+            for result, output_file, orientation in zip(
+                results, output_files, orientations
+            ):
                 save_result(
-                    result=result, output_file=output_file, store_probs=args.store_probs, orientation=orientation
+                    result=result,
+                    output_file=output_file,
+                    store_probs=store_probs,
+                    orientation=orientation,
                 )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    main(**args.__dict__)
