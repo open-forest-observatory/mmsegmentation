34a375ba64fe9e6d22b4bfed44a21451be7e222f
David Russell <djrussell@ucdavis.edu>
2025-06-18 14:03:50 +0000

    Fix issue with 'local_rank' parameter in training

--- tools/train.py
+++ tools/train.py
@@ -44,20 +44,21 @@ def parse_args():
         default="none",
         help="job launcher",
     )
-    # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
-    # will pass the `--local-rank` parameter to `tools/train.py` instead
-    # of `--local_rank`.
     parser.add_argument("--local_rank", "--local-rank", type=int, default=0)
     args = parser.parse_args()
-    if "LOCAL_RANK" not in os.environ:
-        os.environ["LOCAL_RANK"] = str(args.local_rank)
 
     return args
 
 
 def main(
-    config, work_dir=None, launcher="none", cfg_options={}, amp=False, resume=False
+    config, work_dir=None, launcher="none", cfg_options={}, amp=False, resume=False, local_rank=0
 ):
+    # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
+    # will pass the `--local-rank` parameter to `tools/train.py` instead
+    # of `--local_rank`.
+    if "LOCAL_RANK" not in os.environ:
+        os.environ["LOCAL_RANK"] = str(args.local_rank)
+
     # load config
     cfg = Config.fromfile(config)
     cfg.launcher = launcher
